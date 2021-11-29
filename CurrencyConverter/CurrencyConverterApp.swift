9ed97a189d2a4aba57e600e18918f997f810962b
iamsadcopy <iamsadcopy@gmail.com>
2021-11-29 13:02:43 +0300

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Pavel Moskvichev on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
